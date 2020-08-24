38db259a0878c745bb162c8e088f3a22231218b6
acharyaP03 <acharyap03@gmail.com>
2020-08-24 17:58:49 +1000

    Authorize user to message

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -27,6 +27,24 @@ exports.loginRequired = function(req, res, next){
 }
 //make sure we get the correct user - Authrization
 exports.ensureCorrectUser = function(req, res, next){
-    
+    try{
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.SECRET_KEY, function(err, payload){
+            // onlh allow user if the payload id is equals to users id
+            if(payload && payload.id === req.params.id){
+                return next();
+            }else{
+                return next({
+                    status: 401,
+                    message: "Access Unauthorized."
+                })
+            }
+        })
+    }catch(e){
+        return next({
+            status: 401,
+            message: "Access Unauthorized."
+        })
+    }
 }
 
