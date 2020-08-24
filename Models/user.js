06a85376c802c6711150cad12706b262d2c53e3a
acharyaP03 <acharyap03@gmail.com>
2020-08-24 10:56:56 +1000

    mogoose hook pre save and compare

--- Models/user.js
+++ Models/user.js
@@ -19,4 +19,27 @@ const userSchema = new mongoose.Schema({
     profileImageUrl: {
         type: String
     }
+});
+
+//since the password cannot be saved on plain text, we will run mongoose hook to encrypt password before saving. below is the function.
+
+userSchema.pre("save", async function(next){
+    try{
+        //here we are hashing only once, if user hasnt change password.
+        //
+        if(!this.isModified("password")){
+            next()
+        }
+        let hashedPassword = await bcrypt.hash(this.password, 10);
+        //hash the password and assign it to the user shcema password field.
+        this.password = hashedPassword;
+        return next();
+    }catch(err){
+        //if any error, send to the error controller.
+        return next(err);
+    }
 })
+
+cosnt User = mongoose.model("User", userSchema);
+
+module.exports = User;
